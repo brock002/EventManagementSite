9b96d0abc2df28e5e90e7cd4ea689eadfdc55f43
Biswajit Deka <utpaldeka102@gmail.com>
2020-06-04 14:06:02 +0530

    registered model EventImage to admin

--- EventManagement/events/admin.py
+++ EventManagement/events/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from events.models import Category, City, Event, Follower
+from events.models import *
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(City)
 admin.site.register(Event)
 admin.site.register(Follower)
+admin.site.register(EventImage)
