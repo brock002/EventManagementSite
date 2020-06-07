d1dbf14fcccca5e291473bb057c2fc0e749d3a42
Biswajit Deka <utpaldeka102@gmail.com>
2020-06-08 00:06:56 +0530

    Added password reset through email

--- EventManagement/EventManagement/urls.py
+++ EventManagement/EventManagement/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('events/', include('events.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
