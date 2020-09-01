9d933b9bced71d7e8304b4adfca416ba2f71a970
Biswajit Deka <utpaldeka102@gmail.com>
2020-09-01 15:37:00 +0530

    added share button

--- EventManagement/events/static/events/myscript.js
+++ EventManagement/events/static/events/myscript.js
@@ -1,3 +1,13 @@
+$("#copybtn").tooltip('disable');
+var clipboard = new ClipboardJS('#copybtn', {
+    container: document.getElementById('shareButtonModal')
+});
+clipboard.on('success', function() {
+    $("#copybtn").tooltip('enable');
+    $("#copybtn").tooltip('show');
+    $("#copybtn").tooltip('disable');
+});
+
 function scrollDivRight(id) {
     var RtBtn = document.getElementById(id);
     RtBtn.parentElement.parentElement.firstElementChild.scrollBy({
@@ -15,5 +25,5 @@ function scrollDivLeft(id) {
 }
 
 $(function() {
-    $('[data-toggle="tooltip"]').tooltip()
+    $('.alwaysOnTooltips').tooltip()
 })
